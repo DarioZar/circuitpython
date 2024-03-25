9c13e72e2a8eacb5996e5f69e994dbd9d4d53559
Dario Zarcone <dariozarcone@gmail.com>
2024-03-25 16:28:37 +0100

    Add ulab support and removed PortalBase and Request from frozen libs

--- ports/atmel-samd/boards/matrixportal_m4/mpconfigboard.mk
+++ ports/atmel-samd/boards/matrixportal_m4/mpconfigboard.mk
@@ -16,10 +16,10 @@ CIRCUITPY_AESIO = 0
 CIRCUITPY_FLOPPYIO = 0
 CIRCUITPY_PARALLELDISPLAY = 0
 CIRCUITPY_SHARPDISPLAY = 0
-CIRCUITPY_ULAB = 0
+CIRCUITPY_ULAB = 1
 
 # Include these Python libraries in firmware.
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_PortalBase
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_PortalBase
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_ESP32SPI
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
